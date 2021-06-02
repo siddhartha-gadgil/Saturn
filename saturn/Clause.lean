2d0623c4e5f85c0b935c60f50f80272d2f95499b
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-06-02 16:58:32 +0530

    mapping clauses into branches

--- saturn/Clause.lean
+++ saturn/Clause.lean
@@ -5,21 +5,28 @@ def Clause(n : Nat) : Type := (Fin n) → Option Bool
 def restrict{α : Type}(n : Nat)(fn: (Fin (Nat.succ n)) → α)(arg: Fin n) : α :=
   fn (Fin.mk (Nat.succ arg.val) (succ_lt_succ arg.isLt))
 
-#check restrict (5) (fun k => (k.val % 2 == 0))
+def eqClause (n: Nat): (Clause n) → (Clause n) → Bool := 
+  match n with
+    | 0 => fun c1 => (fun c2 => true)
+    | k + 1 => 
+      fun c1 => (fun c2 => eqClause k (restrict k c1) (restrict k c2)) 
 
-def induce{α : Type}(n : Nat)(fn : (Fin n → α))(zeroVal : α)(arg: Fin (n + 1)) : α :=
+instance {n: Nat} : BEq (Clause n) :=   BEq.mk (eqClause n)
+
+def induce{α : Type}{n : Nat}(fn : (Fin n → α))(zeroVal : α)(arg: Fin (n + 1)) : α :=
   match arg with
     | Fin.mk 0 _ => zeroVal
     | Fin.mk (k + 1) witness =>
       let pred := Fin.mk k witness
       fn (pred)
 
-def testRestrictClause(n: Nat): (Clause (n + 1)) → (Clause n) := restrict (n)
-
-#check testRestrictClause (6)
 
 def branchClause {n: Nat} (branch: Bool) (clause : Clause (n + 1)) : Option (Clause n) :=
   if (clause (Fin.mk 0 (zeroLtSucc n))) == some (branch) then
     none
   else
     some (restrict n clause)
+
+def branchMap  {n: Nat} (branch: Bool)(clauses : List (Clause (n  + 1))) 
+  : List (Clause n) :=
+    List.eraseDups (List.filterMap (branchClause branch) clauses)
