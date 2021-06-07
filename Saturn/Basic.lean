83f82d96ca60e18eeacbf4d4bdd2048f2cd35ff0
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-06-07 10:56:10 +0530

    separated theorems, some packaging

--- Saturn/Basic.lean
+++ Saturn/Basic.lean
@@ -10,11 +10,7 @@ def dropHead{α : Type}(n : Nat) : (Fin (Nat.succ n) → α) → Fin n →  α :
     fun arg =>
       fn (plusOne n arg)
 
-def lem1{α : Type}(n: Nat)(zeroVal : α)(j: Fin n)(g: (Fin (succ n)) → α) 
-        : (dropHead
-       n g j) = g (plusOne n j) := by
-        rfl
-        done
+
         
 
 def eqClause (n: Nat): (Clause n) → (Clause n) → Bool := 
@@ -159,103 +155,5 @@ def dlppSAT (n: Nat): (List (Clause n)) →  Option (Solution n) :=
             | some ((j, b)) =>
                 let permuted := clauses.map (transposeZero j)
                 ((dlppSAT k (branchMap b permuted)).map (prepend k b)).map (transposeZero j)
-
--- theorems and experiments
-
-theorem zeroLenClsEql : ∀ (cl1: Clause 0), ∀ (cl2: Clause 0) ,  (cl1 = cl2) := 
-  fun cl1 =>
-    fun cl2 =>
-      funext (
-        fun (x : Fin 0) =>
-          match x with 
-            | ⟨_, h⟩ => absurd h (notLtZero _)
-      )
-
-theorem succEq(k: Nat)(l: Nat) : (k = l) →  (succ k = succ l):= by
-  intro h
-  apply congrArg
-  assumption
-  done 
-
-theorem succNotZero(n : Nat) : ((succ n) = 0) → False := by
-  simp
-  done
-
-theorem zeroNotSucc(n : Nat) : (0 = (succ n)) → False := by
-  simp
-  done
-
-theorem succInjective(k: Nat)(l: Nat) : (succ k = succ l) → k = l := by
-  intro h
-  injection h
-  assumption
-  done
-
-theorem prependPlusOne{α: Type}(n : Nat)(zeroVal : α)(fn : (Fin n → α))(j: Fin n):
-  prepend n zeroVal fn (plusOne n j) = fn j :=
-    -- let l1 : prepend n zeroVal fn (Fin.mk (succ j.val) 
-    --   (succ_lt_succ j.isLt)) = fn (Fin.mk j.val j.isLt) := by rfl
-    -- let l2 : (plusOne n j) = Fin.mk (succ j.val) (succ_lt_succ j.isLt) := by rfl
-    -- let indFn: Fin (n + 1) → α := 
-    --   fun p1j => prepend n zeroVal fn p1j
-    -- let l3 : prepend n zeroVal fn (plusOne n j) =
-    --    prepend n zeroVal fn (Fin.mk (succ j.val) (succ_lt_succ j.isLt)) := congrArg indFn l2
-    -- let l4 := Eq.trans l3 l1
-    let l4 : prepend n zeroVal fn (plusOne n j) = fn (Fin.mk j.val j.isLt) := by rfl
-    let l5 :  Fin.mk j.val j.isLt = j := by
-      apply Fin.eqOfVeq
-      rfl
-      done
-    let l6 := congrArg fn l5
-    Eq.trans l4 l6
-  
-
-theorem dropOnePrepend{α : Type}(n : Nat)(zeroVal : α)(fn : (Fin n → α))(j: Fin n) : 
-    dropHead
-   n (prepend n zeroVal fn) j = fn j := 
-        let lem1 : dropHead n (prepend n zeroVal fn) j = prepend n zeroVal fn (plusOne n j) := by rfl
-        Eq.trans lem1 (prependPlusOne n zeroVal fn j)
-
-def indc {α: Type} (zeroVal : α) (fn: Nat → α) : Nat → α :=
-  fun n =>
-    match n with
-    | 0 => zeroVal
-    | n + 1 => fn (n)
-  
-theorem lemInd{α: Type}(n: Nat)(zeroVal: α)(fn: Nat → α) : indc zeroVal fn (succ n) = fn n := by rfl 
-
-def boolBranch(b: Bool)(n: Nat) := 
-  match b with
-  | true => n 
-  | false => n + 1
-
-theorem boolBranchTest(n: Nat) : boolBranch false n = n + 1 := by rfl 
-
-inductive NEq(k: Nat)(l: Nat) where
-  | AreEq (pf : k = l) : NEq k l
-  | AreUneq (contra : (k = l) → False) : NEq k l
-
-def decNEq(k: Nat)(l: Nat): NEq k l :=
-  match k with
-    | 0 =>
-      match l with
-      | 0 =>
-        let lem : 0 = 0 := by rfl  
-        NEq.AreEq lem
-      | succ m => NEq.AreUneq (zeroNotSucc m)
-    | n + 1 => 
-      match l with
-      | 0 => NEq.AreUneq (succNotZero n)
-      | m + 1 =>
-        let pred := decNEq n m
-        match pred with
-        | NEq.AreEq pf =>
-          NEq.AreEq (congrArg succ pf)
-        | NEq.AreUneq contra =>
-          NEq.AreUneq (fun h => contra (succInjective n m h))
-
-
-
--- scratch
 
 
---
+++ Saturn/FinSeq.lean
@@ -0,0 +1,100 @@
+import Saturn.Basic
+open Nat
+
+-- theorems about finite sequences
+
+theorem dropPlusOne{α : Type}(n: Nat)(zeroVal : α)(j: Fin n)(g: (Fin (succ n)) → α) 
+        : (dropHead
+       n g j) = g (plusOne n j) := by
+        rfl
+        done
+
+theorem zeroLenClsEql : ∀ (cl1: Clause 0), ∀ (cl2: Clause 0) ,  (cl1 = cl2) := 
+  fun cl1 =>
+    fun cl2 =>
+      funext (
+        fun (x : Fin 0) =>
+          match x with 
+            | ⟨_, h⟩ => absurd h (notLtZero _)
+      )
+
+theorem succEq(k: Nat)(l: Nat) : (k = l) →  (succ k = succ l):= by
+  intro h
+  apply congrArg
+  assumption
+  done 
+
+theorem succNotZero(n : Nat) : ((succ n) = 0) → False := by
+  simp
+  done
+
+theorem zeroNotSucc(n : Nat) : (0 = (succ n)) → False := by
+  simp
+  done
+
+theorem succInjective(k: Nat)(l: Nat) : (succ k = succ l) → k = l := by
+  intro h
+  injection h
+  assumption
+  done
+
+theorem prependPlusOne{α: Type}(n : Nat)(zeroVal : α)(fn : (Fin n → α))(j: Fin n):
+  prepend n zeroVal fn (plusOne n j) = fn j :=
+    let l1 : prepend n zeroVal fn (plusOne n j) = fn (Fin.mk j.val j.isLt) := by rfl
+    let l2 :  Fin.mk j.val j.isLt = j := by
+      apply Fin.eqOfVeq
+      rfl
+      done
+    let l3 := congrArg fn l2
+    Eq.trans l1 l3
+  
+
+theorem dropOnePrepend{α : Type}(n : Nat)(zeroVal : α)(fn : (Fin n → α))(j: Fin n) : 
+    dropHead
+   n (prepend n zeroVal fn) j = fn j := 
+        let dropPlusOne : dropHead n (prepend n zeroVal fn) j = prepend n zeroVal fn (plusOne n j) := by rfl
+        Eq.trans dropPlusOne (prependPlusOne n zeroVal fn j)
+
+
+
+-- scratch : miscellaneous theorems
+
+
+def indc {α: Type} (zeroVal : α) (fn: Nat → α) : Nat → α :=
+  fun n =>
+    match n with
+    | 0 => zeroVal
+    | n + 1 => fn (n)
+  
+theorem lemInd{α: Type}(n: Nat)(zeroVal: α)(fn: Nat → α) : indc zeroVal fn (succ n) = fn n := by rfl 
+
+def boolBranch(b: Bool)(n: Nat) := 
+  match b with
+  | true => n 
+  | false => n + 1
+
+theorem boolBranchTest(n: Nat) : boolBranch false n = n + 1 := by rfl 
+
+inductive NEq(k: Nat)(l: Nat) where
+  | AreEq (pf : k = l) : NEq k l
+  | AreUneq (contra : (k = l) → False) : NEq k l
+
+def decNEq(k: Nat)(l: Nat): NEq k l :=
+  match k with
+    | 0 =>
+      match l with
+      | 0 =>
+        let lem : 0 = 0 := by rfl  
+        NEq.AreEq lem
+      | succ m => NEq.AreUneq (zeroNotSucc m)
+    | n + 1 => 
+      match l with
+      | 0 => NEq.AreUneq (succNotZero n)
+      | m + 1 =>
+        let pred := decNEq n m
+        match pred with
+        | NEq.AreEq pf =>
+          NEq.AreEq (congrArg succ pf)
+        | NEq.AreUneq contra =>
+          NEq.AreUneq (fun h => contra (succInjective n m h))
+
