c4901e0049546dda0300318b647ae9ce57b48528
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-08-16 16:50:27 +0530

    N queens, runs slowly

--- Saturn.lean
+++ Saturn.lean
@@ -1,8 +1,10 @@
 import Saturn.Examples
 import Saturn.DPLL
+import Saturn.NQueens
 
 def main : IO UInt32 := do
   IO.println "Hello, SATurn!"
   IO.println (solve eg1Statement).toString
   IO.println (solve eg2Statement).toString
+  IO.println (solve (queensClauses 5)).toString
   return 0
