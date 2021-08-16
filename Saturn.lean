b706771e1f5a66dfee3dc838238b17bf2b7f9a7a
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-08-16 19:10:43 +0530

    parameter n for n-queens

--- Saturn.lean
+++ Saturn.lean
@@ -2,9 +2,19 @@ import Saturn.Examples
 import Saturn.DPLL
 import Saturn.NQueens
 
-def main : IO UInt32 := do
-  IO.println "Hello, SATurn!"
+def printSolution {n dom : Nat}: (clauses : FinSeq dom (Clause n)) →  IO Unit :=
+  match n with
+  | 0 => fun _ => pure ()
+  | l + 1 => 
+    fun clauses => 
+      do
+        IO.println (solve clauses).toString
+        return ()
+
+def main (args: List String) : IO UInt32 := do
+  let n := Option.getD (bind (args.head?) (String.toNat?)) 0
+  IO.println "Hello from SATurn!"
   IO.println (solve eg1Statement).toString
   IO.println (solve eg2Statement).toString
-  IO.println (solve (queensClauses 5)).toString
+  printSolution (queensClauses n)
   return 0
