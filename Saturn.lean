f71bf2390bca33ba229c3aef7e4c142cb61a17d8
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-08-16 09:18:22 +0530

    running binary for test

--- Saturn.lean
+++ Saturn.lean
@@ -1,2 +1,8 @@
-def main : IO Unit :=
-  IO.println "Hello, world!"
+import Saturn.Examples
+import Saturn.DPLL
+
+def main : IO UInt32 := do
+  IO.println "Hello, SATurn!"
+  IO.println (solve eg1Statement).toString
+  IO.println (solve eg2Statement).toString
+  return 0
