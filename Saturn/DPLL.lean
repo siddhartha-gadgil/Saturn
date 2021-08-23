7fc3c9af60225f794394877d74cf64960b33f3b2
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-08-23 16:41:59 +0530

    tail recursive restriction data

--- Saturn/DPLL.lean
+++ Saturn/DPLL.lean
@@ -75,28 +75,40 @@ def restrictionDataAux{domHead domAccum dom n: Nat}(branch: Bool)
 def restrictionData{dom n: Nat}(branch: Bool)(focus: Nat)(focusLt : focus < n + 1):
     (clauses: FinSeq dom (Clause (n + 1))) →   
         RestrictionData branch focus focusLt clauses := 
-      match dom with 
-      | 0 => fun clauses =>  
-        let eqn : clauses = FinSeq.empty := funext (fun j => funext (fun w => nomatch w))
-        match clauses, eqn with
-        | .(FinSeq.empty), rfl =>
+        fun clauses =>
           let rc : RestrictionClauses branch focus focusLt FinSeq.empty := 
               ⟨0, FinSeq.empty, 
                 fun k w => nomatch w, 
                 fun k w => nomatch w, fun k w => nomatch w, fun k w => nomatch w⟩
-          ⟨rc,
-          ⟨fun k w => nomatch w⟩,
-          ⟨fun k w => nomatch w⟩,
-          ⟨fun k w => nomatch w⟩, 
-          ⟨fun k w => nomatch w⟩⟩
-      | l + 1 => fun clauses => 
-        let res := prependResData branch focus focusLt (tail clauses) 
-          (restrictionData branch focus focusLt (tail clauses))
-          (head clauses)
-        let lem := headTail clauses
-        by
-          rw (Eq.symm lem)
-          exact res
+          let rd : RestrictionData branch focus focusLt FinSeq.empty := ⟨rc,
+            ⟨fun k w => nomatch w⟩,
+            ⟨fun k w => nomatch w⟩,
+            ⟨fun k w => nomatch w⟩, 
+            ⟨fun k w => nomatch w⟩⟩
+          restrictionDataAux branch focus focusLt clauses FinSeq.empty 
+              (Nat.add_zero dom) rd clauses (concatEmptySeq clauses)
+      -- match dom with 
+      -- | 0 => fun clauses =>  
+      --   let eqn : clauses = FinSeq.empty := funext (fun j => funext (fun w => nomatch w))
+      --   match clauses, eqn with
+      --   | .(FinSeq.empty), rfl =>
+      --     let rc : RestrictionClauses branch focus focusLt FinSeq.empty := 
+      --         ⟨0, FinSeq.empty, 
+      --           fun k w => nomatch w, 
+      --           fun k w => nomatch w, fun k w => nomatch w, fun k w => nomatch w⟩
+      --     ⟨rc,
+      --     ⟨fun k w => nomatch w⟩,
+      --     ⟨fun k w => nomatch w⟩,
+      --     ⟨fun k w => nomatch w⟩, 
+      --     ⟨fun k w => nomatch w⟩⟩
+      -- | l + 1 => fun clauses => 
+      --   let res := prependResData branch focus focusLt (tail clauses) 
+      --     (restrictionData branch focus focusLt (tail clauses))
+      --     (head clauses)
+      --   let lem := headTail clauses
+      --   by
+      --     rw (Eq.symm lem)
+      --     exact res
 
 def containmentLift{dom n: Nat}(clauses : FinSeq dom (Clause (n + 1)))(cntn : Containment clauses):
           SatSolution (cntn.imageSeq) → SatSolution clauses := 
