8fce87d677fc79873718189381afc57f10970a66
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-12-02 10:08:06 +0530

    clean up solution lift

--- Saturn/LiftSolution.lean
+++ Saturn/LiftSolution.lean
@@ -15,73 +15,45 @@ def pullBackSolution{dom n: Nat}(branch: Bool)(focus : Nat)(focusLt : focus < n 
         ((j : Nat) → (jw : j < rc.codom) → clauseSat (rc.restClauses.coords j jw) valuation) → 
         (j : Nat) → (jw : j < dom) →  
           clauseSat (clauses.coords j jw) (FinSeq.vec (insert branch n focus focusLt valuation.coords)) := 
-        fun valuation pf =>
-          fun k w => 
-            let splitter := (rc.forward k w)
-            match eq:splitter with
-            | none => 
-              let lem1 : (clauses.coords k w).coords focus focusLt = some branch := dp.dropped k w eq
-              let lem2 : insert branch n focus focusLt valuation.coords focus focusLt = branch := by 
-                apply insert_at_focus
-                done
-              let lem3 : (clauses.coords k w).coords focus focusLt = 
-                some (insert branch n focus focusLt valuation.coords focus focusLt) := 
-                by
-                  rw [lem1]
-                  apply (congrArg some)
-                  exact Eq.symm lem2
-                  done
-              let lem4 : ((clauses.coords k w).coords focus focusLt) = some (
-                    (FinSeq.vec (insert branch n focus focusLt 
-                      valuation.coords)).coords focus focusLt) := 
-                      by 
-                        rw [seq_to_vec_coords (insert branch n focus focusLt 
-                      valuation.coords)]
-                        rw [lem3]
-                        done
-              ⟨focus, focusLt, lem4⟩
-            | some j => 
-              let bound := rc.forwardWit k w 
-              let jWitAux : boundOpt rc.codom (some j) := by
-                rw [←  eq]
-                exact bound
-                done
-              let jWit : j < rc.codom := jWitAux
-              let lem1 := fr.forwardRelation k w j eq jWit
-              let ⟨i, iw, vs⟩ := pf j jWit
-              let lem2 : (rc.restClauses.coords j jWit).coords i iw = 
-                      some (valuation.coords i iw) := vs
-              let lem3 : delete focus focusLt ((clauses.coords k w).coords) i iw =
-                  some (valuation.coords i iw) := 
-                    by
-                    rw [←  lem2]
-                    rw [lem1]
-                    done
-              let lem4 : delete focus focusLt ((clauses.coords k w).coords) i iw =
-                ((clauses.coords k w).coords) (skip focus i) (skip_le_succ iw) := by
-                  rfl
-                  done
-              let lem5 : insert branch n focus focusLt valuation.coords 
-                              (skip focus i) (skip_le_succ iw) =
-                                  valuation.coords i iw := by
-                                    apply insert_at_image
-                                    done
-              let lem6 : ((clauses.coords k w).coords) (skip focus i) (skip_le_succ iw) =
-                          some (insert branch n focus focusLt valuation.coords 
-                              (skip focus i) (skip_le_succ iw)) := by
-                              rw [← lem4]
-                              rw [lem3]
-                              rw [lem5]
-                              done
-              let lem7 : ((clauses.coords k w).coords (skip focus i) (skip_le_succ iw)) =
-                some (
-                  ((FinSeq.vec (insert branch n focus focusLt valuation.coords)).coords 
-                    (skip focus i) (skip_le_succ iw))) := 
-                      by
-                        rw [seq_to_vec_coords (insert branch n focus focusLt valuation.coords)]
-                        rw [lem6]
-                        done
-              ⟨skip focus i, skip_le_succ iw, lem7⟩
+        by
+          intro valuation pf k w  
+          let fwdOpt := (rc.forward k w)
+          match eq:fwdOpt with
+          | none => 
+            apply (fun vs => ⟨focus, focusLt, vs⟩)
+            let resolve : (clauses.coords k w).coords focus focusLt = some branch := dp.dropped k w eq
+            rw [resolve]
+            let insfoc : insert branch n focus focusLt valuation.coords focus focusLt = branch := by 
+              apply insert_at_focus
+            let sv : coords (vec (insert branch n focus focusLt (coords valuation))) =
+                  insert branch n focus focusLt valuation.coords := by
+                    apply seq_to_vec_coords
+            rw [sv]
+            rw [insfoc]
+          | some j => 
+            let bound := rc.forwardWit k w 
+            let jWitAux : boundOpt rc.codom (some j) := by
+              rw [←  eq]
+              exact bound
+            let jWit : j < rc.codom := jWitAux
+            let fwdEq := fr.forwardRelation k w j eq jWit
+            let ⟨i, iw, vs⟩ := pf j jWit
+            apply (fun val => ⟨skip focus i, skip_le_succ iw, val⟩)
+            let sv : coords (vec (insert branch n focus focusLt valuation.coords)) = 
+                            insert branch n focus focusLt (valuation.coords) := by
+                              apply seq_to_vec_coords
+            rw [sv] 
+            let insImage : insert branch n focus focusLt valuation.coords 
+                            (skip focus i) (skip_le_succ iw) =
+                                valuation.coords i iw := by
+                                  apply insert_at_image
+            rw [insImage]
+            let delSkip : delete focus focusLt ((clauses.coords k w).coords) i iw =
+              ((clauses.coords k w).coords) (skip focus i) (skip_le_succ iw) := by
+                rfl
+            rw [← delSkip]
+            rw [fwdEq]
+            exact vs                      
 
 -- pulling back a tree
 def pullBackTree{dom n: Nat}(branch: Bool)(focus: Nat )(focusLt : focus <  (n + 2))
@@ -144,33 +116,26 @@ def pullBackResTree{dom n: Nat}(branch: Bool)(focus: Nat )(focusLt : focus <  (n
     (clauses: Vector (Clause (n + 2)) dom)(rc: RestrictionClauses branch focus focusLt clauses) 
     (np : NonPosReverse rc) (rr: ReverseRelation rc) : 
         ResolutionTree rc.restClauses (contradiction (n + 1)) → 
-            LiftedResTree branch focus focusLt clauses := fun rpf =>
+            LiftedResTree branch focus focusLt clauses := by
+            intro rpf
             let pbt := pullBackTree branch focus focusLt clauses rc np rr 
                                 (contradiction (n + 1)) rpf 
             match pbt.topFocus, pbt.nonPos, pbt.provedTree with 
             | none, _, tree => 
                 have lem :
-                  FinSeq.vec (insert none (Nat.add n 1) focus focusLt 
+                  vec (insert none (Nat.add n 1) focus focusLt 
                     (contradiction (n + 1)).coords) =
                     contradiction (n + 2) := by
                       rw [contradiction_insert_none focus focusLt]
                       apply coords_eq_implies_vec_eq
                       apply seq_to_vec_coords
-                      done            
-                let t : ResolutionTree clauses (contradiction (n + 2)) := by
-                            rw [← lem]
-                            exact tree
-                            done
-                LiftedResTree.contra t
+                rw [lem] at tree
+                exact LiftedResTree.contra tree
             | some b, ineq, tree =>
                 have lemPar : Not (b = branch) := fun hyp => ineq (congrArg some hyp)
                 let par : b = not branch := not_eq_implies_eq_not branch lemPar
-                let t : ResolutionTree clauses (unitClause (n + 1) (not branch) focus focusLt) := 
-                          by
-                            rw [← par]
-                            exact tree
-                            done
-                LiftedResTree.unit t
+                rw [par] at tree
+                exact LiftedResTree.unit tree
 
 -- transporting proof from a subset of clauses to a larger set of clauses
 def transportResTree{l1 l2 n : Nat}(clauses1 : Vector (Clause (n + 1)) l1)
@@ -185,14 +150,12 @@ def transportResTree{l1 l2 n : Nat}(clauses1 : Vector (Clause (n + 1)) l1)
                           let ⟨i, iw, eqn⟩ := embed ind bd                        
                           ResolutionTree.assumption i iw _ (by 
                             rw [eqn]
-                            exact te
-                            done)
+                            exact te)
                       | ResolutionTree.resolve left right .(top) leftTree rightTree join =>
                           let leftPf2 := transportResTree clauses1 clauses2 embed left
                               leftTree 
                           let rightPf2 := transportResTree clauses1 clauses2 embed right
                               rightTree  
-                          let tree := ResolutionTree.resolve left right top
+                          ResolutionTree.resolve left right top
                                       leftPf2 rightPf2 join
 
-                          tree
